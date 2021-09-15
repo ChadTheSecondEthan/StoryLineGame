f98699858220f7376c2fcdaca3ea60c45caab105
Student <Student@118-66402.local>
2021-09-15 09:51:50 -0400

    Started working on menu

--- src/com/chad/storylinegame/gameState/Menu.java
+++ src/com/chad/storylinegame/gameState/Menu.java
@@ -1,12 +1,18 @@
 package com.chad.storylinegame.gameState;
 
 import com.chad.engine.gameState.GameState;
+import com.chad.engine.ui.Text;
 
 public class Menu extends GameState {
 
+    public Menu() {
+        super();
+
+        readEntitiesFromFile("state/menu.txt");
+    }
+
     @Override
     public void init() {
-
     }
 
 }
