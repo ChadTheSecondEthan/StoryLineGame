716f1eaee98d3d84358d0bf36a9c82447a5c9dba
Student <Student@118-66402.local>
2021-09-15 09:45:03 -0400

    First commit

---
+++ src/com/chad/storylinegame/gameState/InGame.java
@@ -0,0 +1,12 @@
+package com.chad.storylinegame.gameState;
+
+import com.chad.engine.gameState.GameState;
+
+public class InGame extends GameState {
+
+    @Override
+    public void init() {
+
+    }
+
+}
---
+++ src/com/chad/storylinegame/gameState/Menu.java
@@ -0,0 +1,12 @@
+package com.chad.storylinegame.gameState;
+
+import com.chad.engine.gameState.GameState;
+
+public class Menu extends GameState {
+
+    @Override
+    public void init() {
+
+    }
+
+}
