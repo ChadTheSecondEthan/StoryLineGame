794e75e71ad14dd89383d8d6168d34f20417754c
Student <Student@118-67398.local>
2021-09-17 09:52:07 -0400

    remove collision code and put in game engine

--- src/com/chad/storylinegame/entity/Player.java
+++ src/com/chad/storylinegame/entity/Player.java
@@ -62,57 +62,8 @@ public class Player extends Entity {
         else if (getY() < 0)
             setY(0);
 
-        checkCollisions(tileMap.getTileBounds(1, 0));
-    }
-
-    @Override
-    public void checkCollisions(Rectf other) {
-        float x = getX();
-        float y = getY();
-
-        float right = x + width;
-        float bottom = y + height;
-        float xmiddle = x + width * 0.5f;
-        float ymiddle = y + height * 0.5f;
-
-        float obottom = other.y + other.h;
-        float oright = other.x + other.w;
-        float oxmiddle = other.x + other.w * 0.5f;
-        float oymiddle = other.y + other.y * 0.5f;
-
-        // make sure the bounds are intersecting
-        if (!(right > other.x && x < oright && bottom > other.y && y < obottom))
-            return;
-
-        float colleft = right - other.x;
-        float colright = oright - x;
-
-        float coltop = obottom - y;
-        float colbottom = bottom - other.y;
-
-        float colx = Math.min(colleft, colright);
-        float coly = Math.min(coltop, colbottom);
-
-        if (colx > coly) {
-            if (dir == Direction.UP)
-                setY(obottom);
-            else if (dir == Direction.DOWN)
-                setY(other.y - height);
-        }
-        else {
-            if (dir == Direction.RIGHT)
-                setX(other.x - width);
-            else if (dir == Direction.LEFT)
-                setX(oright);
-        }
-
-
-//        if (bottom > other.y && bounds.y < obottom) {
-//            if (right > other.x && right < omiddle) {
-//                setX(other.x - bounds.w);
-//            } else if (bounds.x < oright && bounds.x > omiddle)
-//                setX(oright);
-//        }
+        // update collisions
+        checkTileMapCollisions(tileMap);
     }
 
 }
